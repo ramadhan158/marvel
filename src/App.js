3aaed253b40f0894fe806e134220af8b55f98add
ramadhan <ramadhansahri158@gmail.com>
2020-12-02 23:29:52 +0700

    Req API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Marvel from './Marvel'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const apikey = '8a41cb1c97646600edf2a1f2626f6f1a';
+  const ts = '1605539483000';
+  const hash = 'fa671960ed91ebe8038f30063c34b4c6';
+
+  const [hero, setHero] = useState();
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState(undefined);
+
+  useEffect(() => {
+      getAPI();
+  }, [query]);
+
+  const getAPI = async () => {
+    const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}&name=${query}`)
+    const data = await response.json();
+    const result = await data.data.results[0]
+    setHero(result)
+    console.log(result)
+  }
+
+    const updateSearch = (e) => {
+      setSearch(e.target.value);
+    }
+
+    const getSearch = (e) => {
+      e.preventDefault()
+      if (search === "") {
+        setQuery(null)  
+      }else{
+        setQuery(search)
+      }
+      setSearch("")
+    }
+
+    console.log(hero)
+
+    return(
+        <div className="App">
+            <form onSubmit={getSearch}>
+                <input value={search} onChange={updateSearch}/>
+                <button>Cari</button>
+            </form>
+            <div>
+              <Marvel name="hhh"/>
+            </div>
+        </div>
+    )
+};
 
 export default App;
---
+++ src/Marvel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Marvel = ({name}) => {
+	return(
+		<>
+		<div>{name}</div>
+		<div>des</div>
+		<div>img</div>
+		</>
+	);
+};
+
+export default Marvel;

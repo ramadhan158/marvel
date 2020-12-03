249767676df74b1eaf78897a69b0d6d2486ffe53
ramadhan <ramadhansahri158@gmail.com>
2020-12-03 22:23:17 +0700

    tambah getAPIComics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { useEffect, useState } from 'react'
-import Marvel from './Marvel'
+import Marvel from './Marvel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const apikey = '8a41cb1c97646600edf2a1f2626f6f1a';
   const ts = '1605539483000';
   const hash = 'fa671960ed91ebe8038f30063c34b4c6';
 
+  //hero state 
   const [hero, setHero] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState(undefined);
 
-  useEffect(() => {
-      getAPI();
+  //comic state
+  const [comic, setComic] = useState([]);
+
+  useEffect(() => { 
+      // get api hero
+      const getAPIHero = async () => {
+        try{
+          const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}&name=${query}`)
+          const data = await response.json();
+          const result = await data.data.results
+          setHero(result)  
+        } catch(err) {
+          console.log(err)
+        }
+      }
+      getAPIHero();
+
+      // get api comic
+      const getAPIComics = async () => {
+        try{
+          const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}&name=${query}`)
+          const data = await response.json();
+          const result = await data.data.results
+          setHero(result)  
+        } catch(err) {
+          console.log(err)
+        }
+      }
+
   }, [query]);
 
-  const getAPI = async () => {
-    const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}&name=${query}`)
-    const data = await response.json();
-    const result = await data.data.results
-    setHero(result)
-  }
 
     const updateSearch = (e) => {
       setSearch(e.target.value);
@@ -35,19 +58,23 @@ const App = () => {
       setSearch("")
     }
 
+    console.log(hero)
+
     return(
-        <div className="App">
+      <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <form onSubmit={getSearch}>
                 <input value={search} onChange={updateSearch}/>
                 <button>Cari</button>
             </form>
+        </nav>
             <div>
                 {hero.map(he => (
-                <Marvel name={he.name} des={he.description} />
+                // key salah
+                <Marvel key={he.name} name={he.name} des={he.description} img={he.thumbnail.path}/>
                 ))}
             </div>
-            
-        </div>
+      </>
     )
 };
 
--- src/Marvel.js
+++ src/Marvel.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Marvel = ({name, des, img}) => {
 	return(
-		<>
-		<div>{name}</div>
-		<div>{des}</div>
-		<div>{img}</div>
-		</>
+		<div className="media m-4">
+			<img className="mr-3" src={img + "/standard_amazing.jpg"} alt=""/>
+				<div className="media-body">
+					<h5 className="mt-0">{name}</h5>
+					<p>{des}</p>
+				</div>
+		</div>
 	);
 };
 

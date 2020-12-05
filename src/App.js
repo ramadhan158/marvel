a9ecda08c31609b07e274f650c14a905d8463572
ramadhan <ramadhansahri158@gmail.com>
2020-12-05 23:25:29 +0700

    adding comics search

--- src/App.js
+++ src/App.js
@@ -13,67 +13,66 @@ const App = () => {
   const [query, setQuery] = useState(undefined);
 
   //comic state
-  const [comic, setComic] = useState([]);
+  const [comics, setComics] = useState();
 
   useEffect(() => { 
-      // get api hero
+      // get hero
       const getAPIHero = async () => {
         try{
           const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}&name=${query}`)
           const data = await response.json();
           const result = await data.data.results
-          setHero(result)  
+          setHero(result)
         } catch(err) {
           console.log(err)
         }
       }
       getAPIHero();
 
-      // get api comic
-      const getAPIComics = async () => {
-        try{
-          const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}&name=${query}`)
-          const data = await response.json();
-          const result = await data.data.results
-          setHero(result)  
-        } catch(err) {
-          console.log(err)
-        }
-      }
-
   }, [query]);
 
-
-    const updateSearch = (e) => {
-      setSearch(e.target.value);
+  //getting comics
+  if (hero.length === 1) {
+    const getComics = async () => {
+      const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${hero[0].id}/comics?apikey=${apikey}&ts=${ts}&hash=${hash}`)
+      const data = await response.json()
+      const comics = await data.data.results
+      setComics(comics)
     }
+    getComics()
+  }
 
-    const getSearch = (e) => {
-      e.preventDefault()
-      if (search === "") {
-        setQuery(null)  
-      }else{
-        setQuery(search)
-      }
-      setSearch("")
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault()
+    if (search === "") {
+      setQuery(null)  
+    }else{
+      setQuery(search)
     }
+    setSearch("")
+  }
 
-    console.log(hero)
+  // konsol iki lho su, run en lak metu terus console.log e
+  console.log(comics)
 
-    return(
-      <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <form onSubmit={getSearch}>
-                <input value={search} onChange={updateSearch}/>
-                <button>Cari</button>
-            </form>
+  return(
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <form className="form-inline" onSubmit={getSearch}>
+          <input className="form-control" type="text" placeholder="Search" aria-label="Search" value={search} onChange={updateSearch} />
+            <button className="btn btn-primary">Cari</button>
+        </form>
         </nav>
-            <div>
-                {hero.map(he => (
-                // key salah
-                <Marvel key={he.name} name={he.name} des={he.description} img={he.thumbnail.path}/>
-                ))}
-            </div>
+          <div>
+            {hero.map(he => (
+              // key salah
+              <Marvel key={he.name} name={he.name} des={he.description} img={he.thumbnail.path}/>
+            ))}
+          </div>
       </>
     )
 };
